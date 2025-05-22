e213f140e3fdecaeb6920302c2ca26d99833326d
Oleksandra Koshyk <sakoshik1984@gmail.com>
2025-05-22 10:35:52 +0200

    correct messages

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const moveFile = async () => {
       await fs.stat(sourceFile);
     } catch (err) {
       if (err.code === 'ENOENT') {
-        throw new Error('Source file is not excit');
+        throw new Error('Source file does not exist');
       }
       throw err;
     }
@@ -33,7 +33,10 @@ const moveFile = async () => {
         await fs.rename(sourceFile, finalPathWithdestination);
       } else {
         if (destination.endsWith('/')) {
-          throw new Error('Destination ends with /, but is not a directory');
+          throw new Error(
+            // eslint-disable-next-line max-len
+            'Destination path is expected to be a directory but does not exist or is not recognized as one',
+          );
         }
         await fs.rename(sourceFile, destination);
       }
